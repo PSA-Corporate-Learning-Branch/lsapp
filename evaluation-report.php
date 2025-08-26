1eec2d82992309df934e5e698469cf4d012a0de1
Ben Novak <ben.novak@gov.bc.ca>
2025-08-26 17:13:34 +0000

    add text responses

--- evaluation-report.php
+++ evaluation-report.php
@@ -144,13 +144,17 @@ function createChartForRadio($question, $responses) {
                     
     // iterate through answer options
     foreach($answer_options as $option) {
+        $percent_total = round(($option['value'] / $responses[$question]['total']) * 100);
         $html .=    '<div class="row align-items-center">';
         $html .=        '<div class="col-4">';
-        $html .=            '<p style="font-size: smaller;">' . $option['name'] . '</p>';
+        $html .=            '<p style="font-size: smaller;" class="my-1">' . $option['name'] . '</p>';
         $html .=        '</div>';
-        $html .=        '<div class="col-8">';
-        $html .=            '<div class="progress" role="progressbar" aria-label="' . $option['name'] . '" aria-valuenow="' . $option['value'] . '" aria-valuemin="0" aria-valuemax="100">';
-        $html .=                '<div class="progress-bar text-bg-success" style="width: ' . $option['value'] . '%">' . $option['value'] . '%</div>';
+        $html .=        '<div class="col-2">';
+        $html .=            '<p style="font-size: smaller;" class="my-1">' . $option['value'] . '</p>';
+        $html .=        '</div>';
+        $html .=        '<div class="col-6">';
+        $html .=            '<div class="progress" role="progressbar" aria-label="' . $option['name'] . '" aria-valuenow="' . $percent_total . '" aria-valuemin="0" aria-valuemax="100">';
+        $html .=                '<div class="progress-bar text-bg-success" style="width: ' . $percent_total . '%">' . $percent_total . '%</div>';
         $html .=            '</div>';
         $html .=        '</div>';
         $html .=    '</div>';
@@ -169,11 +173,11 @@ function createChartForRadio($question, $responses) {
             datasets: [{
                 data: " . json_encode($chart_values) . ",
                 backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)',
-                'rgba(32, 185, 57, 1)',
-                'rgba(157, 47, 167, 1)',
+                '#971B2F',
+                '#5F2167',
+                '#e3a82b',
+                '#007864',
+                '#234075',
                 ],
                 hoverOffset: 4
             }]
@@ -184,7 +188,24 @@ function createChartForRadio($question, $responses) {
     
 }
 
+function createTextResponses($question, $responses) {
+    global $response_map;
 
+    $html =    '<div class="card my-3" >';
+    $html .=        '<h2 class="m-3">What was tricky?</h2>';
+    $html .=        '<details>';
+    $html .=            '<summary class="m-3">View responses <span class="badge text-bg-secondary">' . count($responses[$question]) . '</span></summary>';
+    $html .=            '<ul class="list-group m-3">';
+    foreach ($responses[$question] as $response) {
+        $html .=            '<li class="list-group-item">' . $response . '</li>';
+    }
+    $html .=            '</ul>';
+    $html .=        '</details>';
+    $html .=    '</div>';
+
+    return $html;
+
+}
 
 
 
@@ -214,49 +235,34 @@ function createChartForRadio($question, $responses) {
 
 
 
-<pre><?php print_r($compiled_responses) ?></pre>
+<pre>
+    <?php //print_r($compiled_responses) ?>
+</pre>
 
 
 
-<div class="container-lg p-lg-5 p-4 bg-light-subtle">
+<div class="container-lg p-lg-5 p-4 bg-light-subtle shadow-sm">
     <h1>Courses Evaluation Report</h1>
      
-    <div class="row justify-content-md-center">
-        
-        <div class="col-4">
-            
-            
-
-            <?php
-            foreach($compiled_responses as $question => $response) {
-                if ($response_map[$question]['inputType'] == 'radio') {
+    <?php
+    foreach($compiled_responses as $question => $response) {
+        if ($response_map[$question]['inputType'] == 'radio') {
+            echo '<div class="row justify-content-md-center">';
+                echo '<div class="col-4">';
                     echo createChartForRadio($question, $compiled_responses);
-                }
-            }
-            ?>
-        </div>
-
-        <div class="col-4">
-
-            <div class="card my-3" > <!-- style="width: 18rem;" -->
-                <h2 class="m-3">What was tricky?</h2>
-                <details>
-                    <summary class="m-3">View details</summary>
-                    <!-- <div class="card-body"> -->
-                    <ul class="list-group m-3">
-                        <li class="list-group-item">An item</li>
-                        <li class="list-group-item">A second item</li>
-                        <li class="list-group-item">A third item</li>
-                        <li class="list-group-item">A fourth item</li>
-                        <li class="list-group-item">And a fifth one</li>
-                    </ul>
-                    <!-- </div> -->
-                </details>
-
-            </div>
-
-        </div>
-    </div>
+                echo '</div>';
+            echo '</div>';
+        }
+        else if ($response_map[$question]['inputType'] == 'text') {
+            echo '<div class="row justify-content-md-center">';
+                echo '<div class="col-8">';
+                    echo createTextResponses($question, $compiled_responses);
+                echo '</div>';
+            echo '</div>';
+        }
+    }
+    ?>
+    
 
 </div>
 
