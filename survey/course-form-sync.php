5b7ee391d2d1fbe041b3d54167860ae8e224c8da
Ben Novak <ben.novak@gov.bc.ca>
2025-11-07 20:07:26 +0000

    add courses with form id to config in pending status

--- survey/course-form-sync.php
+++ survey/course-form-sync.php
@@ -2,10 +2,34 @@
 opcache_reset();
 $path = '../inc/lsapp.php';
 require($path); 
+require('eval-functions.php');
+
+$config = getConfig();
 
 $courses = getCourses();
 
-$formcourses = array();
-// foreach ($courses as $course) {
-    
-// }
+// since we're primarily just looking to see if a form id in our course
+// exists in our config, we can create an array of config form ids to
+// more easily check against
+$surveys = array();
+foreach ($config as $c) {
+    $surveys[] = $c['formId'];
+}
+
+// go through our courses lookive for active courses with form ids added
+foreach ($courses as $course) {
+    if ($course[1] == 'Active' && !empty($course[46])) {
+        // if the form id doesn't exist in our config, add in pending status
+        if (!in_array($course[46], $surveys)) {
+            $config[] = [
+                'formId' => $course[46],
+                'status' => 'pending',
+                'courseId' => $course[0]
+            ];
+        }
+        
+    }
+}
+
+// save our updated config
+updateConfig($config);
