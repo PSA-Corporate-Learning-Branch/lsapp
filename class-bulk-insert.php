08d8291fd42456569b3729ccdb9c2c04bd930b5e
Ben Novak <ben.novak@gov.bc.ca>
2025-11-24 19:51:26 +0000

    add tooltip instructions for facilitating field

--- class-bulk-insert.php
+++ class-bulk-insert.php
@@ -99,6 +99,12 @@ $allpeople = getPeopleAll($filteractive = true);
 			<?php endif ?>
 			<div class="col-md-6">
 				<label for="AddFacilitating-1" class="form-label">Facilitating</label>
+				<a href="#" class="link-secondary" data-bs-toggle="tooltip" data-bs-html="true" data-bs-title="<small>Enter or search facilitator from the dropdown, then select Add.<br>Add facilitators one at a time.</small>">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+  						<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  						<path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+					</svg>
+				</a>
 				<div class="input-group">
 					<input class="form-control" list="people-list" id="AddFacilitating-1" name="AddFacilitating" value="">
 					<button id="AddButton-1" type="button" class="btn btn-secondary">Add</button>
@@ -261,6 +267,9 @@ $allpeople = getPeopleAll($filteractive = true);
 	}
 	
 	AddFacilitation(1);
+
+	const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+	const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 </script>
 
