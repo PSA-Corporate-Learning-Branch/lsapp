a5b543f2aa0efc7f6cdaa3b848720eba22e91f78
Ben Novak <ben.novak@gov.bc.ca>
2026-03-03 00:29:21 +0000

    limit request to once per minute and re-direct with alert if under

--- survey/get-responses.php
+++ survey/get-responses.php
@@ -14,13 +14,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $survey_config = getConfigSurvey($form_id);
 
     // check lastResponsesUpdated
-
-    print_r($survey_config);
+    $time_since_last_sync = time() - $survey_config['lastResponsesUpdated'];
+    if ($time_since_last_sync < 60) {
+        AlertManager::addAlert('warning', "Responses requested $time_since_last_sync seconds ago. Please wait a full minute before next sync.");
+        header('Location: ./evaluation-report.php?formId=' . $form_id);
+            exit;
+    }
+    
 
     // decrypt form secret
-    $form_secret = EncryptionHelper::decrypt($config['formSecret']);
-
-    $responses_config = $config; // replaced by $survey_config
+    $form_secret = EncryptionHelper::decrypt($survey_config['formSecret']);
 
     $params = [
         'format' => 'json',
@@ -47,30 +50,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $context = stream_context_create($options);
 
-    $new_responses = file_get_contents($submissions_export_endpoint, false, $context);
+    // $new_responses = file_get_contents($submissions_export_endpoint, false, $context);
 
     // if we're only getting recent responses, check if there were any
 
     $response_file = $data_path . $form_id . '-test.json';
+    
     // check if file already exists
-    if (file_exists($response_file)) {
-        $file_contents = file_get_contents($response_file);
-        $current_responses = json_decode($file_contents, true);
+    // if (file_exists($response_file)) {
+    //     $file_contents = file_get_contents($response_file);
+    //     $current_responses = json_decode($file_contents, true);
 
-        // extract new responses and add to current responses
-    }
+    //     // extract new responses and add to current responses
+    // }
 
-    file_put_contents($response_file, $response);
+    // save responses to file
+    // file_put_contents($response_file, $new_responses);
 
-    // $response_data = json_decode($response, true);
-
+    // update last sync time
     $survey_config['lastResponsesUpdated'] = time();
 
     // update the config file for this form
     // updateConfigByFormId($form_id, $survey_config);
-    // return $responses_config;
 
 
+    echo '<pre>';
+    print_r($time_since_last_sync);
+    echo '</pre>';
 
 
 
